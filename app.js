6472073b04af11e44f21cf58c798b099d8fc004d
Lennox Fei <feif4@mcmaster.ca>
2018-01-30 14:04:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ class faction{
 function totalwar(fac1,fac2){
     return new Promise((res,rej)=>{
         let vic = fac2, growth = 0;
-        while(fac1.strength && fac2.strength){
+        while(fac1.strength && fac2.strength){//just playing with the async feature
             fac1.strength--;
             fac2.strength--;
             growth++;
         }
+        //can be optimized to just take growth as min(fac1.strength,fac2.strength)
+        //optimization: vic assigned to the max(fac1,fac2)
         if(fac1.strength>0){
             vic=fac1;
         }
