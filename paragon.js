5b5d2bf58e0f910e0023b7b5ca01e7b12b71c246
Lennox Fei <feif4@mcmaster.ca>
2018-02-08 11:37:23 -0500

    Update paragon.js
    Additional wrapper functions to extend support and provide peace of mind

--- paragon.js
+++ paragon.js
@@ -24,3 +24,27 @@ class paragon{
         return this.exp(this.q);
     }
 }
+
+//having just the object itself without some other abstraction and support is probably a really dumb idea
+//so here is some wrappers for your async dependency needs
+
+function paraval(val, paragon, i){
+    return paragon.push(i,val);
+}//value
+
+function parathread(func, paragon, i, p=[]){
+    return paragon.push(i,func.apply(this,p));
+}//function and thread
+
+function paracal(func, paragon, i, p=[]){
+    c = (result)=>{
+        return paragon.push(i,result);
+    }
+    p.push(c);
+    func.apply(this,p);
+}//callback
+
+function parapro(pro, paragon, i, err = (e)=>{console.log(e);}){
+    pro.then((value){paragon.push(i,value);})
+    .catch(err);
+}//promise
