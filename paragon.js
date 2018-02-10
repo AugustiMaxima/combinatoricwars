2dfb64edb43ea2caea9ab258767c434bd12cf6b8
Lennox Fei <feif4@mcmaster.ca>
2018-02-10 01:53:36 -0500

    Update paragon.js
    An experimental approach at abstracting the base to support both promise and callbacks

--- paragon.js
+++ paragon.js
@@ -1,4 +1,3 @@
-
 //using subscriber pattern to resolve the problem of multiple aynsc dependencies
 
 class paragon{
@@ -8,7 +7,8 @@ class paragon{
         this.n = num_p;
         this.c = cur_p;
         this.q = q_p;
-        this.cb = callback;
+        this.res = callback;
+        this.rej;
         this.r = false; //whether wait is finished
         if(this.c === this.n){
             resolv();
@@ -24,18 +24,32 @@ class paragon{
         else
             return this.c;
     }
+    //used to attach resolve and reject calls. can also be used to bind callbacks
+    promise(res,rej=null){
+        this.res = res;
+        this.rej = rej;//TODO: Implement reject for callback& promise wrappers so this actually makes sense
+    }
+    
     //as the expression depends on the dependencies noted in q; the expression will take q as a container for the parameters
     resolv(){
         this.val = this.exp(this.q);
         this.r = true;
-        if(this.cb){//implementation of callback support
-            return this.cb(this.val);
+        if(this.res){//implementation of callback support
+            return this.res(this.val);
         }
         else{
             return this.val;
         }
     }
 }
+
+//an attempt at wrapping the base in a promise
+function propara(p){
+    return new Promise((res,rej)=>{
+        p.promise(res,rej);
+    });
+}
+
 
 //having just the object itself without some other abstraction and support is probably a really dumb idea
 //so here is some wrappers for your async dependency needs
